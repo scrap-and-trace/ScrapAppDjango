656a2ca309d9dff7a8acb5c721ac7cc20944cc04
Gregor Campbell <gregorcampbell@Gregors-Laptop.local>
2023-03-20 14:50:27 +0000

    added session authentication

--- scrap/scrap/settings.py
+++ scrap/scrap/settings.py
@@ -143,7 +143,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_AUTHENTICATION_CLASSES': ['knox.auth.TokenAuthentication', ]
+    'DEFAULT_AUTHENTICATION_CLASSES': ['knox.auth.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication',
+
+                                       ]
     # https://www.django-rest-framework.org/api-guide/pagination/#example
 
 
